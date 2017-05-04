8b4c771600639d0d74fff9de76f3d30793298c17
Kane York <kyork@student.42.us.org>
2017-05-04 16:14:44 -0700

    transform into breadth-first search

--- srcs/search/search_workall.c
+++ srcs/search/search_workall.c
@@ -6,21 +6,45 @@
 /*   By: kyork <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/04 13:28:00 by kyork             #+#    #+#             */
-/*   Updated: 2017/05/04 15:04:45 by kyork            ###   ########.fr       */
+/*   Updated: 2017/05/04 16:12:55 by kyork            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "type.h"
 #include <ft_printf.h>
+#include <limits.h>
 
-void		search_workall(t_farm *f)
+static size_t	select_next(t_farm *f)
+{
+	size_t	best_idx;
+	size_t	best_len;
+	size_t	i;
+	t_path	*it;
+
+	best_idx = 0;
+	best_len = INT_MAX;
+	i = 0;
+	while (i < f->pathq.item_count)
+	{
+		it = (t_path*)ft_ary_get(&f->pathq, i);
+		if (it->item_count < best_len)
+		{
+			best_idx = i;
+			best_len = it->item_count;
+		}
+		i++;
+	}
+	return (best_idx);
+}
+
+void			search_workall(t_farm *f)
 {
 	size_t	idx;
 	t_path	p;
 
-	while (f->pathq.item_count != 0)
+	while (f->pathq.item_count != 0 && f->paths.item_count < 20)
 	{
-		idx = f->pathq.item_count - 1;
+		idx = select_next(f);
 		p = *(t_path*)ft_ary_get(&f->pathq, idx);
 		ft_printf("working path #%ld\n", idx);
 		search_work_path(f, &p);
